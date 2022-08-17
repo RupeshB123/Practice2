b555657d9ed7da0192085e0742e0330898fd72ba
RupeshB123 <roopiesh77@gmail.com>
2022-08-17 12:20:22 +0530

    J unit test

--- src/test/java/com/exa/serviceImpl/test/EmployeeServiceImplTest.java
+++ src/test/java/com/exa/serviceImpl/test/EmployeeServiceImplTest.java
@@ -1,27 +1,59 @@
 package com.exa.serviceImpl.test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
+import org.powermock.api.mockito.PowerMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.exa.binding.LoginForm;
 import com.exa.model.Employee;
+import com.exa.repository.EmployeeRepository;
 
+@Service
 public class EmployeeServiceImplTest 
 {
 	
+	 EmployeeRepository mockRepo = PowerMockito.mock(EmployeeRepository.class);
+
+	 @Autowired
+		private EmployeeRepository empRepo;
+
 
 	@Test
-	public String addEmployee(Employee emp) 
+	public void addEmployee() 
 	{
 
-		//Employee employee = new Employee(0, null, null, null, null, null);
+		Employee employee = new Employee(101, "nm", 55.55, 54444l, "mmm", "kkk");
+//	
 		
-		if(emp.getEmpName()==null )
-		{
-			return "Enter correct details" ;
-		}
+//		PowerMockito.when(mockRepo.save(emp)).thenReturn(employee);
 		
-		Employee employee = empRepo.save(emp);
+		 Employee save = empRepo.save(employee);
 		
-		return "Employee added successfully";
+		assertNotNull(save); 
+		
+		//return "Employee added successfully";
+	}
+	
+	
+//	@Test
+	public void loginCheck(LoginForm loginForm) 
+	{
+		
+		
+		Employee employee = mockRepo.findByUserNameAndPazzword(loginForm.getUserName() , loginForm.getPazzword());
+
+		assertNotNull(employee);
+		
+		//EmployeeRepository mockRepo = new 
+
+		
+		
+		//return employee;
 	}
 
 }
